54bef99fdd6961a60ebdb9085090410d13714bd0
Adrian <adriadsv@gmail.com>
2025-12-19 09:13:16 -0500

    Proyecto Laravel inicial

---
+++ resources/views/parqueadero.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Parqueadero</title>
+</head>
+<body>
+  <h1>Parqueadero</h1>
+  <p>Esta es mi primera vista en Laravel.</p>
+</body>
+</html>
